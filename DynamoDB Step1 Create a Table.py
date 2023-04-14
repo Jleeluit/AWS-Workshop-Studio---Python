3290112d9db7158fb4d6db23e90068951668e92d
Jason Li <jlee77711@outlook.com>
2023-04-14 15:47:58 +0000

    DynamnoDBStep1

---
+++ DynamoDB Step1 Create a Table.py
@@ -0,0 +1,38 @@
+#This Python Script is from AWS workshop studio
+
+import boto3
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
+table = dynamodb.create_table(
+    TableName='Movies',
+    KeySchema=[
+        {
+            'AttributeName': 'year',
+            'KeyType': 'HASH'  #Partition key
+        },
+        {
+            'AttributeName': 'title',
+            'KeyType': 'RANGE'  #Sort key
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'year',
+            'AttributeType': 'N'
+        },
+        {
+            'AttributeName': 'title',
+            'AttributeType': 'S'
+        },
+
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 10,
+        'WriteCapacityUnits': 10
+    }
+)
+
+print("Table status:", table.table_status)
+
+# USe this command to run the program in the AWS cloud9                   terminal python MoviesCreateTable.py
