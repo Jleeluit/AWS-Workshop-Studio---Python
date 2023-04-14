d0c6399ec3dc3a27eb508292d9813f98f599d73c
Jason Li <124201063+Jleeluit@users.noreply.github.com>
2023-04-14 12:13:59 -0400

    Update DynamoDBStep3Create,Read,Update,and DeleteanItem.py

--- DynamoDBStep3Create,Read,Update,and DeleteanItem.py
+++ DynamoDBStep3Create,Read,Update,and DeleteanItem.py
@@ -32,3 +32,101 @@ response = table.put_item(
 
 print("PutItem succeeded:")
 print(json.dumps(response, indent=4, cls=DecimalEncoder))
+
+
+
+
+#Update An Item
+import boto3
+import json
+import decimal
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+
+table = dynamodb.Table('Movies')
+
+title = "The Big New Movie"
+year = 2015
+
+response = table.update_item(
+    Key={
+        'year': year,
+        'title': title
+    },
+    UpdateExpression="set #info_rating=:r, #info_plot=:p, #info_actors=:a",
+    ExpressionAttributeNames={
+        '#info_rating': 'info.rating',
+        '#info_plot': 'info.plot',
+        '#info_actors': 'info.actors'
+    },
+    ExpressionAttributeValues={
+        ':r': decimal.Decimal(5.5),
+        ':p': "Everything happens all at once.",
+        ':a': ["Larry", "Moe", "Curly"]
+    },
+    ReturnValues="UPDATED_NEW"
+)
+
+print("UpdateItem succeeded:")
+print(json.dumps(response, indent=4, cls=DecimalEncoder))
+
+
+
+#Delete An Item
+
+import boto3
+from botocore.exceptions import ClientError
+import json
+import decimal
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+
+table = dynamodb.Table('Movies')
+
+title = "The Big New Movie"
+year = 2015
+
+print("Attempting a conditional delete...")
+
+try:
+    response = table.delete_item(
+        Key={
+            'year': year,
+            'title': title
+        },
+        ConditionExpression="#info_rating <= :val",
+        ExpressionAttributeNames= {
+            "#info_rating": "info.rating"
+        },
+        ExpressionAttributeValues= {
+            ":val": decimal.Decimal(5)
+        }
+    )
+except ClientError as e:
+    if e.response['Error']['Code'] == "ConditionalCheckFailedException":
+        print(e.response['Error']['Message'])
+    else:
+        raise
+else:
+    print("DeleteItem succeeded:")
+    print(json.dumps(response, indent=4, cls=DecimalEncoder))
