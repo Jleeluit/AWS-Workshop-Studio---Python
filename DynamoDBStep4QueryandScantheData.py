ece22e40962cb241c9a28c4e1ac19d1e64aead3f
Jason Li <jlee77711@outlook.com>
2023-04-14 16:20:18 +0000

    DynamnoStep4

---
+++ DynamoDBStep4QueryandScantheData.py
@@ -0,0 +1,111 @@
+#Step 4 - Query and Scan the Data
+
+
+import boto3
+import json
+import decimal
+from boto3.dynamodb.conditions import Key, Attr
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+
+table = dynamodb.Table('Movies')
+
+print("Movies from 1985")
+
+response = table.query(
+    KeyConditionExpression=Key('year').eq(1985)
+)
+
+for i in response['Items']:
+    print(i['year'], ":", i['title'])
+
+#Query - All movies Released in a Year
+import boto3
+import json
+import decimal
+from boto3.dynamodb.conditions import Key, Attr
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return str(o)
+        return super(DecimalEncoder, self).default(o)
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
+table = dynamodb.Table('Movies')
+
+print("Movies from 1992 - titles A-L, with genres and lead actor")
+
+response = table.query(
+    ProjectionExpression="#yr, title, info.genres, info.actors[0]",
+    ExpressionAttributeNames={ "#yr": "year" }, # Expression Attribute Names for Projection Expression only.
+    KeyConditionExpression=Key('year').eq(1992) & Key('title').between('A', 'L')
+)
+
+for i in response[u'Items']:
+    print(json.dumps(i, cls=DecimalEncoder))
+    
+    
+    
+    
+#    The scan method reads every item in the entire table and returns all the data in the table. You can provide an optional filter_expression so that only the items matching your criteria are returned. However, the filter is applied only after the entire table has been scanned.
+
+The following program scans the entire Movies table, which contains approximately 5,000 items. The scan specifies the optional filter to retrieve only the movies from the 1950s (approximately 100 items) and discard all the others.
+
+import boto3
+import json
+import decimal
+from boto3.dynamodb.conditions import Key, Attr
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+
+table = dynamodb.Table('Movies')
+
+fe = Key('year').between(1950, 1959)
+pe = "#yr, title, info.rating"
+# Expression Attribute Names for Projection Expression only.
+ean = { "#yr": "year", }
+esk = None
+
+
+response = table.scan(
+    FilterExpression=fe,
+    ProjectionExpression=pe,
+    ExpressionAttributeNames=ean
+    )
+
+for i in response['Items']:
+    print(json.dumps(i, cls=DecimalEncoder))
+
+while 'LastEvaluatedKey' in response:
+    response = table.scan(
+        ProjectionExpression=pe,
+        FilterExpression=fe,
+        ExpressionAttributeNames= ean,
+        ExclusiveStartKey=response['LastEvaluatedKey']
+        )
+
+    for i in response['Items']:
+        print(json.dumps(i, cls=DecimalEncoder))
